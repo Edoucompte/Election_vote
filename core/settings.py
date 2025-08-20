a7fbb79752ceab67c9e14917a134498121d43736
edouardo <edouardoaf2@gmail.com>
2025-08-20 01:26:32 +0100

    suite loggin config

--- core/settings.py
+++ core/settings.py
@@ -129,6 +129,41 @@ AUTH_USER_MODEL = "vote.CustomUser"
 # Logging
 import logging
 
-l= logging.getLogger('django.db.backends')
-l.setLevel(logging.DEBUG)
-l.addHandler(logging.StreamHandler())
+#l= logging.getLogger('django.db.backends')
+#l.setLevel(logging.DEBUG)
+#l.addHandler(logging.StreamHandler())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
