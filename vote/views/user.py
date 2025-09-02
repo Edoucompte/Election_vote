3bbd84490ae4ac4137f383868a488a13cb9bfcd7
Mannau95 <jeanbaptistekintonou@gmail.com>
2025-09-02 08:52:42 +0100

    permissions

--- vote/views/user.py
+++ vote/views/user.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-
+from rest_framework.authentication import SessionAuthentication
 from vote.filters.users import UserFilter
 from vote.models import CustomUser
 from vote.serializers import CustomUserSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 class CustomUserModelViewSet(viewsets.ModelViewSet):
@@ -17,3 +19,5 @@ class CustomUserModelViewSet(viewsets.ModelViewSet):
     filter_fields = ( 'is_active', 'is_superviseur', 'sexe')
     search_fields = ('first_name', 'last_name', 'email')
     ordering_fields = ('first_name', )
+   # authentication_classes = [SessionAuthentication]
+   # permission_classes = [IsAuthenticated]
