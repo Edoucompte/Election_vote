7b467753ad864d911e49ef561710da2364572018
edouardo <edouardoaf2@gmail.com>
2025-09-12 08:34:54 +0100

    custom jwt setting for expiration time

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,7 +151,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.AllowAny',
@@ -168,6 +169,13 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),  # Set access token to expire in 60 minutes
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=1),   # Set refresh token to expire in 7 days
+        "ROTATE_REFRESH_TOKENS": True,                 # Optional: Rotate refresh tokens on use
+        "BLACKLIST_AFTER_ROTATION": True,              # Optional: Blacklist old refresh tokens
+    }
 
 AUTH_USER_MODEL = "vote.CustomUser"
 
