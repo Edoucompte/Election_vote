f4d48ad88ab1fb058cd955ee7cdd8e3fa3f42715
edouardo <edouardoaf2@gmail.com>
2025-09-16 00:03:55 +0100

    anteriortie de heure de debut et de fin

--- vote/serializers/election.py
+++ vote/serializers/election.py
@@ -1,3 +1,4 @@
+from  datetime import datetime, timezone, timedelta
 from rest_framework import serializers
 from vote.models import Election
 
@@ -6,3 +7,13 @@ class ElectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Election
         fields = '__all__'
+
+    def validate(self, data):
+        print("Inside validate :")
+        now_time = datetime.now(timezone(timedelta(hours=1))) # utc +1
+        print("data", type(data['begin_date']), "now time",now_time)
+        if data['begin_date'] > data['end_date'] :
+            raise serializers.ValidationError("La date de debut doit etre anterieur a la date de fin")
+        elif data['begin_date'] < now_time:
+            raise serializers.ValidationError("La date de debut doit etre posterieur a la date actuelle")
+        return data
